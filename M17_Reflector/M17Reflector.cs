15c89e33ccde71c8dd691bafe2eb4499bc88ba63
php <ko4uyj@gmail.com>
2024-09-21 00:28:55 -0500

    Add copyright headers to files that did not have them

--- M17_Reflector/M17Reflector.cs
+++ M17_Reflector/M17Reflector.cs
@@ -1,11 +1,30 @@
-﻿using Common;
+﻿/*
+* MMDVM_Reflector - Common
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* 
+* Copyright (C) 2024 Caleb, KO4UYJ
+* 
+*/
+
+using Common;
 using Common.Api;
 using Newtonsoft.Json;
 using Serilog;
 using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace M17_Reflector
